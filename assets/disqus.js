9dd637e23f1961561c5953617dcb75bcd19249ff
D-side <side.spb@gmail.com>
2014-09-12 13:49:42 +0400

    Back gradient; disqus refactored

--- assets/disqus.js
+++ assets/disqus.js
@@ -1,5 +1,13 @@
 (function() {
-	function fire() {var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;dsq.src = '//dsideblog.disqus.com/embed.js';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);};
+	function fire() {
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = '//dsideblog.disqus.com/embed.js';
+    var thing = (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]);
+    console.log(thing);
+    thing.appendChild(dsq);
+  };
   if (document.addEventListener) {
     document.addEventListener('DOMContentLoaded', fire);
   } else {
